14ee7ffec0c7c11ac82f293cab70bb5ade44ec20
edenis00 <edenis0072@gmail.com>
2026-08-29 15:59:29 +0100

    fix: change extra option in SettingsConfigDict from "ignore" to "allow"

--- app/config.py
+++ app/config.py
@@ -27,6 +27,6 @@ class Settings(BaseSettings):
     
     IP_API_KEY: str
     
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
     
 settings = Settings()
